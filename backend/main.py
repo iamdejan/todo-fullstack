568a703e10e6f11dc33872e4640f5bf096542cb9
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2024-09-16 12:22:57 +0700

    feat: get and delete item (#2)

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pypika import Table, Query # type: ignore
 from pypika.enums import Order # type: ignore
 from ulid import ULID
-from pydantic_types import ToDoItem, ToDoItemRequest
+from pydantic_types import ToDoItem, CreateToDoItemRequest
 from const import zero_ulid, default_page_limit
 from database_connection_pool import pool
 
@@ -11,6 +11,15 @@ import time
 
 
 server_start_time = time.time()
+todo_item_not_found_code = "TODO_ITEM_NOT_FOUND"
+
+
+def construct_not_found_detail(id: str) -> dict:
+    return {
+        "message": "To-do item not found",
+        "code": todo_item_not_found_code,
+        "value": id
+    }
 
 
 # integrate pool's lifespan with FastAPI
@@ -20,8 +29,10 @@ async def lifespan(app: FastAPI):
     yield
     await pool.close()
 
+
 # inintiate FastAPI
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def root() -> dict:
@@ -29,7 +40,7 @@ async def root() -> dict:
 
 
 @app.get("/todos")
-async def get_all_todos(last_id: str = zero_ulid, limit: int = default_page_limit) -> list:
+async def get_all_todos(last_id: str = zero_ulid, limit: int = default_page_limit) -> list[ToDoItem]:
     todo_items = Table("todo_items")
     q = Query.from_(todo_items).select(todo_items.id, todo_items.title, todo_items.description, todo_items.finished).\
         where(last_id < todo_items.id).\
@@ -47,8 +58,25 @@ async def get_all_todos(last_id: str = zero_ulid, limit: int = default_page_limi
     return results
 
 
+@app.get("/todos/{id}")
+async def get_todo_by_id(id: str) -> ToDoItem | None:
+    todo_items = Table("todo_items")
+    q = Query.from_(todo_items).select(todo_items.id, todo_items.title, todo_items.description, todo_items.finished).\
+        where(todo_items.id == id).\
+        limit(1)
+
+    q_str = str(q)
+    async with pool.connection() as conn:
+        cursor = await conn.execute(q_str)
+        record = await cursor.fetchone()
+        if record is None:
+            raise HTTPException(status_code=404, detail=construct_not_found_detail(id))
+    result = ToDoItem(id=record[0], title=record[1], description=record[2], finished=record[3])
+    return result
+
+
 @app.post("/todos")
-async def create_todo(request: ToDoItemRequest):
+async def create_todo(request: CreateToDoItemRequest):
     item_id = ULID.from_timestamp(server_start_time)
     item_id_str: str = str(item_id)
 
@@ -62,3 +90,28 @@ async def create_todo(request: ToDoItemRequest):
         await conn.execute(q_str)
 
     return {}
+
+
+@app.delete("/todos/{id}")
+async def delete_todo(id: str):
+    todo_items = Table("todo_items")
+    get_query = Query.from_(todo_items).select(todo_items.id, todo_items.title, todo_items.description, todo_items.finished).\
+        where(todo_items.id == id).\
+        limit(1)
+    
+    delete_query = Query.from_(todo_items).delete().\
+        where(todo_items.id == id)
+
+    get_query_str = str(get_query)
+    delete_query_str = str(delete_query)
+    async with pool.connection() as conn:
+        cursor = await conn.execute(get_query_str)
+        record = await cursor.fetchone()
+        if record is None:
+            raise HTTPException(status_code=404, detail=construct_not_found_detail(id))
+
+        # actual delete process
+        await conn.execute(delete_query_str)
+
+    result = ToDoItem(id=record[0], title=record[1], description=record[2], finished=record[3])
+    return result
