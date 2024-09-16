fdb2accb075aa14c4d161921b5a0027172cc4c46
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-16 11:00:04 +0700

    feat: create todo item

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,25 @@
 from contextlib import asynccontextmanager
 from psycopg_pool import AsyncConnectionPool
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from pypika import Table, Query
+from pypika.enums import Order
+from ulid import ULID
 
+import time
 import os
 
-host = "database"
-port = 5432
+load_dotenv()
+
+host = os.environ["POSTGRES_HOST"]
+dbport = int(os.environ["POSTGRES_PORT"])
 dbname = os.environ["POSTGRES_DB"]
 user = os.environ["POSTGRES_USER"]
 password = os.environ["POSTGRES_PASSWORD"]
 
 # initiate connection pool for database
-pool = AsyncConnectionPool(conninfo=F"host=database port=5432 dbname={dbname} user={user} password={password}", open=False)
+pool = AsyncConnectionPool(conninfo=F"host={host} port={dbport} dbname={dbname} user={user} password={password}", open=False)
 
 # integrate pool's lifespan with FastAPI
 @asynccontextmanager
@@ -27,6 +35,50 @@ app = FastAPI(lifespan=lifespan)
 async def root() -> dict:
     return {"message": "Hello world!"}
 
+
+class ToDoItem(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
+    finished: bool
+
+
 @app.get("/todos")
-async def get_all_todos() -> list:
-    return []
+async def get_all_todos(last_id: str = "00000000000000000000000000", limit: int = 10) -> list:
+    todo_items = Table("todo_items")
+    q = Query.from_(todo_items).select(todo_items.id, todo_items.title, todo_items.description, todo_items.finished).\
+        where(last_id < todo_items.id).\
+        orderby(todo_items.id, order=Order.asc).\
+        limit(limit)
+
+    results = []
+    async with pool.connection() as conn:
+        cursor = await conn.execute(str(q))
+        async for record in cursor:
+            result = ToDoItem(id=record[0], title=record[1], description=record[2], finished=record[3])
+            results.append(result)
+
+    return results
+
+
+class ToDoItemRequest(BaseModel):
+    title: str
+    description: str | None = None
+
+
+server_start_time = time.time()
+
+
+@app.post("/todos")
+async def create_todo(request: ToDoItemRequest):
+    item_id = ULID.from_timestamp(server_start_time)
+
+    todo_items = Table("todo_items")
+    q = Query.into(todo_items).\
+        columns(todo_items.id, todo_items.title, todo_items.description, todo_items.finished).\
+        insert(str(item_id), request.title, request.description, False)
+
+    async with pool.connection() as conn:
+        await conn.execute(str(q))
+
+    return {}
